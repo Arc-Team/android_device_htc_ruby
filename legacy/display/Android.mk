cbf566804ccecfc90ccfae06f369c4c59eed2248
José Adolfo Galdámez <josegalre@gmail.com>
2014-06-19 12:01:04 -0600

    Add Display and Medial legacy packages into device
    from https://github.com/TeamNightmare/android_hardware_qcom_media-legacy-ion/commits/cm-11.0
    https://github.com/TeamNightmare/android_hardware_qcom_display-legacy-ion/tree/cm-11.0

---
+++ legacy/display/Android.mk
@@ -0,0 +1,8 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),ruby)
+display-hals := libgralloc libgenlock libcopybit
+display-hals += libhwcomposer liboverlay libqdutils
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
+include $(call all-named-subdir-makefiles,$(display-hals))
+endif
---
+++ legacy/media/Android.mk
@@ -0,0 +1,5 @@
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),ruby)
+
+include $(call all-subdir-makefiles)
+
+endif
