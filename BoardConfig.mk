9e8c3abae319154d997ddb039cef1b8d7d925350
JoseGalRe <josegalre@gmail.com>
2014-06-19 11:06:10 -0600

    Initial Import from TeamNightmare branch cm-11.0 for Kernel 3.0.101
    All credits to Sportsstar89 https://github.com/TeamNightmare/android_device_htc_ruby/tree/cm-11.0

---
+++ BoardConfig.mk
@@ -0,0 +1,76 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# inherit from msm8660-common
+-include device/htc/msm8660-common/BoardConfigCommon.mk
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := ruby
+
+# Kernel
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=ruby no_console_suspend=1
+BOARD_KERNEL_PAGE_SIZE := 2048
+TARGET_KERNEL_CONFIG := ruby_defconfig
+
+# TI Chip
+-include device/htc/ruby/ti_chip.mk
+
+# QCOM GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ruby
+
+# AUDIO
+BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
+BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
+BOARD_USES_SEPERATED_CAMCORDER := true  # use distinct camcorder use cases
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
+
+# NFC
+BOARD_HAVE_NFC := true
+
+# Use libril in the device tree
+BOARD_PROVIDES_LIBRIL := true
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610611712
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2684354048
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Recovery
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_FSTAB = device/htc/ruby/ramdisk/fstab.ruby
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+RECOVERY_FSTAB_VERSION := 2
---
+++ device_ruby.mk
@@ -0,0 +1,142 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherite msm8660-common configs
+$(call inherit-product, device/htc/msm8660-common/msm8660.mk)
+
+# Ramdisk files
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/ramdisk/fstab.ruby:root/fstab.ruby \
+    device/htc/ruby/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/htc/ruby/ramdisk/init.ruby.rc:root/init.ruby.rc \
+    device/htc/ruby/ramdisk/init.ruby.usb.rc:root/init.ruby.usb.rc \
+    device/htc/ruby/ramdisk/ueventd.ruby.rc:root/ueventd.ruby.rc
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    ro.com.google.locationfeatures=1 \
+    dalvik.vm.dexopt-flags=m=y
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/ruby/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.ruby
+
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+# Launcher
+PRODUCT_PACKAGES += \
+    Launcher2
+
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    dhcpcd.conf \
+    hostapd.conf \
+    wpa_supplicant.conf \
+    tiwlan.ini \
+    TQS_D_1.7.ini \
+    TQS_D_1.7_127x.ini \
+    calibrator
+
+# recovery and custom charging
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/recovery/root/sbin/choice_fn:recovery/root/sbin/choice_fn \
+    device/htc/ruby/recovery/root/sbin/power_test:recovery/root/sbin/power_test \
+    device/htc/ruby/recovery/root/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
+    device/htc/ruby/recovery/root/sbin/detect_key:recovery/root/sbin/detect_key \
+    device/htc/ruby/recovery/root/sbin/htcbatt:recovery/root/sbin/htcbatt
+
+# Keylayouts and Keychars
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/keychars/ruby-keypad.kcm:system/usr/keychars/ruby-keypad.kcm \
+    device/htc/ruby/keylayout/atmel-touchscreen.kl:system/usr/keylayout/atmel-touchscreen.kl \
+    device/htc/ruby/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/ruby/keylayout/ruby-keypad.kl:system/usr/keylayout/ruby-keypad.kl
+
+# Input device config
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/idc/atmel-touchscreen.idc:system/usr/idc/atmel-touchscreen.idc \
+    device/htc/ruby/idc/ruby-keypad.idc:system/usr/idc/ruby-keypad.idc
+
+# Sound Configs
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/dsp/soundimage/Sound_MFG.txt:system/etc/soundimage/Sound_MFG.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Original_Recording.txt:system/etc/soundimage/Sound_Original_Recording.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Phone_Original_HP.txt:system/etc/soundimage/Sound_Phone_Original_HP.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Phone_Original_SPK.txt:system/etc/soundimage/Sound_Phone_Original_SPK.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Rec_Landscape.txt:system/etc/soundimage/Sound_Rec_Landscape.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Recording.txt:system/etc/soundimage/Sound_Recording.txt \
+    device/htc/ruby/dsp/soundimage/Sound_Rec_Portrait.txt:system/etc/soundimage/Sound_Rec_Portrait.txt \
+    device/htc/ruby/dsp/soundimage/srsfx_trumedia_51.cfg:system/etc/soundimage/srsfx_trumedia_51.cfg \
+    device/htc/ruby/dsp/soundimage/srsfx_trumedia_movie.cfg:system/etc/soundimage/srsfx_trumedia_movie.cfg \
+    device/htc/ruby/dsp/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
+    device/htc/ruby/dsp/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
+    device/htc/ruby/dsp/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
+    device/htc/ruby/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/ruby/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+    device/htc/ruby/dsp/AIC3254_REG_DualMic_XD.csv:system/etc/AIC3254_REG_DualMic_XD.csv \
+    device/htc/ruby/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
+    device/htc/ruby/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/ruby/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+    device/htc/ruby/dsp/TPA2051_CFG_XB.csv:system/etc/TPA2051_CFG_XB.csv \
+    device/htc/ruby/dsp/TPA2051_CFG_XC.csv:system/etc/TPA2051_CFG_XC.csv
+
+# WiFi
+#$(call inherit-product, hardware/ti/wlan/mac80211/wl127x-wlan-products.mk)
+
+PRODUCT_COPY_FILES += \
+    device/htc/ruby/ti/system/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    device/htc/ruby/ti/system/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
+
+# BT
+$(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# call proprietary setup
+$(call inherit-product-if-exists, vendor/htc/ruby/ruby-vendor.mk)
+
+# HTC Audio Settings
+$(call inherit-product, device/htc/ruby/media_a1026.mk)
+$(call inherit-product, device/htc/ruby/media_htcaudio.mk)
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_DEVICE := ruby
+PRODUCT_NAME := ruby
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := Amaze 4G
+PRODUCT_MANUFACTURER := HTC
