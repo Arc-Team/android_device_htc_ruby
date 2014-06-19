2efb4496086b17a9a01137e7fc5313ecc3a16cc2
José Adolfo Galdámez <josegalre@gmail.com>
2014-06-19 13:28:08 -0600

    Cleanup repo A

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,31 +1,130 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-#
-# This file sets variables that control the way modules are built
-# thorughout the system. It should not be used to conditionally
-# disable makefiles (the proper mechanism to control what gets
-# included in a build is to use PRODUCT_PACKAGES in a product
-# definition file).
-#
+BOARD_VENDOR := htc
 
-# WARNING: This line must come *before* including the proprietary
-# variant, so that it gets overwritten by the parent (which goes
-# against the traditional rules of inheritance).
+TARGET_SPECIFIC_HEADER_PATH := device/htc/ruby/include
 
-# inherit from msm8660-common
--include device/htc/ruby/BoardConfigCommon.mk
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/htc/ruby
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8660
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a8
+
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# FB legacy
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_DISPLAY_VARIANT := ruby
+
+# Audio
+COMMON_GLOBAL_CFLAGS += -DHTC_ACOUSTIC_AUDIO -DLEGACY_QCOM_VOICE -DQCOM_ACDB_ENABLED
+TARGET_QCOM_AUDIO_VARIANT := caf
+BOARD_QCOM_TUNNEL_LPA_ENABLED := false
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/ruby/bluetooth/include
+
+# Camera
+BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DNO_UPDATE_PREVIEW
+BOARD_HAVE_HTC_FFC := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+BOARD_USES_PMEM_CAMERA := true
+
+# Filesystem
+BOARD_VOLD_MAX_PARTITIONS := 36
+
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+
+# Media
+TARGET_QCOM_MEDIA_VARIANT := ruby
+
+# Graphics
+TARGET_USES_ION := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DREFRESH_RATE=60 -DHTC_RGBA_8888_OFFSET
+USE_OPENGL_RENDERER := true
+TARGET_NO_HW_VSYNC := true
+TARGET_DOESNT_USE_FENCE_SYNC := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+BOARD_EGL_NEEDS_FNW := true
+TARGET_DISPLAY_INSECURE_MM_HEAP := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+TARGET_ENABLE_AV_ENHANCEMENTS := true
+TARGET_USES_POST_PROCESSING := true
+TARGET_USES_C2D_COMPOSITION := true
+BOARD_EGL_CFG := device/htc/ruby/configs/egl.cfg
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+
+# Lights
+# legacy LIBLIGHT naming
+TARGET_PROVIDES_LIBLIGHT := true
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# OpenGL
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+# Sensors
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Hardware tunables
+BOARD_HARDWARE_CLASS := device/htc/ruby/cmhw
+
+# Recovery
+USE_SET_METADATA := false
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/htc/ruby/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    app.te \
+    bluetooth.te \
+    device.te \
+    domain.te \
+    drmserver.te \
+    file_contexts \
+    files \
+    file.te \
+    hci_init.te \
+    healthd.te \
+    init.te \
+    init_shell.te \
+    keystore.te \
+    kickstart.te \
+    mediaserver.te \
+    rild.te \
+    surfaceflinger.te \
+    system.te \
+    ueventd.te \
+    untrusted_app.te \
+    vold.te \
+    wpa.te \
+    wpa_socket.te
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := ruby
@@ -36,8 +135,43 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=ruby no_console_sus
 BOARD_KERNEL_PAGE_SIZE := 2048
 TARGET_KERNEL_CONFIG := ruby_defconfig
 
-# TI Chip
--include device/htc/ruby/ti_chip.mk
+# Wifi
+USES_TI_MAC80211 := true
+ifdef USES_TI_MAC80211
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+endif
+
+# Required for newer wpa_supplicant_8_ti versions to fix tethering
+#BOARD_WIFI_SKIP_CAPABILITIES := true
+
+WLAN_MODULES:
+	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
+	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# Bluetooth
+BOARD_WPAN_DEVICE := true
+BOARD_HAVE_BLUETOOTH_TI := true
+
+# FM
+TARGET_PROVIDES_TI_FM_SERVICE := true
 
 # QCOM GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ruby
--- cm.mk
+++ cm.mk
@@ -1,23 +1,21 @@
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# CM Stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Device Stuff
+$(call inherit-product, device/htc/ruby/ruby.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := ruby
 
+# Bootanimation
 TARGET_BOOTANIMATION_NAME := 540
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/htc/ruby/device_ruby.mk)
-
-# Setup device specific product configuration.
+# Device Naming
 PRODUCT_DEVICE := ruby
 PRODUCT_NAME := cm_ruby
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Amaze 4G
 PRODUCT_MANUFACTURER := HTC
 
-# Set build fingerprint / ID / Product Name etc.
+# Set Build Bingerprint / ID / Product Name
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_FINGERPRINT=tmous/htc_ruby/ruby:4.4/KRT16M/373265.3:user/release-keys PRIVATE_BUILD_DESC="2.14.531.3_R2 CL373265 release-keys" BUILD_NUMBER=373265
