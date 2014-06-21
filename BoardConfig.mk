5f80fa10a3df073edc27598413c8eb79d8920f05
José Adolfo Galdámez <josegalre@gmail.com>
2014-06-21 11:47:49 -0600

    Cleanup repo E

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,68 +1,62 @@
-
+# Vendor
 BOARD_VENDOR := htc
-
 TARGET_SPECIFIC_HEADER_PATH := device/htc/ruby/include
 
 # Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8660
 TARGET_NO_BOOTLOADER := true
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/htc/ruby
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8660
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 # Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a8
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := scorpion
+#TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
 
-# Flags
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+# Kernel
+BOARD_KERNEL_BASE := 0x48000000
+BOARD_KERNEL_PAGE_SIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=ruby no_console_suspend=1
+TARGET_KERNEL_SOURCE := kernel/htc/ruby
+TARGET_KERNEL_CONFIG := ruby_defconfig
 
-# FB legacy
-BOARD_EGL_NEEDS_LEGACY_FB := true
-
-# QCOM hardware
+# QCOM
 BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_AUDIO_VARIANT := caf
 TARGET_QCOM_DISPLAY_VARIANT := ruby
+TARGET_QCOM_MEDIA_VARIANT := ruby
 
 # Audio
-COMMON_GLOBAL_CFLAGS += -DHTC_ACOUSTIC_AUDIO -DLEGACY_QCOM_VOICE -DQCOM_ACDB_ENABLED
-TARGET_QCOM_AUDIO_VARIANT := caf
 BOARD_QCOM_TUNNEL_LPA_ENABLED := false
 BOARD_USES_LEGACY_ALSA_AUDIO := true
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
+BOARD_USES_SEPERATED_CAMCORDER := true
+BOARD_USES_SEPERATED_VOICE_SPEAKER := true
+COMMON_GLOBAL_CFLAGS += -DHTC_ACOUSTIC_AUDIO 
+COMMON_GLOBAL_CFLAGS += -DLEGACY_QCOM_VOICE 
+COMMON_GLOBAL_CFLAGS += -DQCOM_ACDB_ENABLED
 
 # Bluetooth
+BOARD_WPAN_DEVICE := true
+BOARD_HAVE_BLUETOOTH_TI := true
 BOARD_HAVE_BLUETOOTH := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/ruby/bluetooth
 
-# Camera
-BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
-COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB -DNO_UPDATE_PREVIEW
-BOARD_HAVE_HTC_FFC := true
-TARGET_DISABLE_ARM_PIE := true
-BOARD_NEEDS_MEMORYHEAPPMEM := true
-USE_DEVICE_SPECIFIC_CAMERA := true
-BOARD_USES_PMEM_CAMERA := true
-
-# Filesystem
-BOARD_VOLD_MAX_PARTITIONS := 36
-
 # GPS
 BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ruby
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
-
-# Media
-TARGET_QCOM_MEDIA_VARIANT := ruby
 
 # Graphics
 TARGET_USES_ION := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DREFRESH_RATE=60 -DHTC_RGBA_8888_OFFSET
 USE_OPENGL_RENDERER := true
 TARGET_NO_HW_VSYNC := true
 TARGET_DOESNT_USE_FENCE_SYNC := true
@@ -73,33 +67,101 @@ BOARD_USE_MHEAP_SCREENSHOT := true
 TARGET_ENABLE_AV_ENHANCEMENTS := true
 TARGET_USES_POST_PROCESSING := true
 TARGET_USES_C2D_COMPOSITION := true
-BOARD_EGL_CFG := device/htc/ruby/prebuilt/lib/egl/egl.cfg
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+BOARD_EGL_CFG := device/htc/ruby/prebuilt/lib/egl/egl.cfg
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK 
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 
+COMMON_GLOBAL_CFLAGS += -DHTC_RGBA_8888_OFFSET
 
-# Lights
-# legacy LIBLIGHT naming
-TARGET_PROVIDES_LIBLIGHT := true
-TARGET_PROVIDES_LIBLIGHTS := true
+# Camera
+BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
+BOARD_HAVE_HTC_FFC := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+USE_DEVICE_SPECIFIC_CAMERA := true
+BOARD_USES_PMEM_CAMERA := true
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB 
+COMMON_GLOBAL_CFLAGS += -DNO_UPDATE_PREVIEW
 
-# OpenGL
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
+# Wifi
+USES_TI_MAC80211 := true
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE = wl12xx_mac80211
+BOARD_SOFTAP_DEVICE := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
 
-# Sensors
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+WLAN_MODULES:
+	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
+	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
+	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+
+TARGET_KERNEL_MODULES += WLAN_MODULES
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610611712
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2684354048
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 36
+
+# Recovery
+USE_SET_METADATA := false
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TARGET_RECOVERY_FSTAB = device/htc/ruby/ramdisk/fstab.ruby
+
+# CWM
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
 # Webkit
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 
-# Hardware tunables
-BOARD_HARDWARE_CLASS := device/htc/ruby/cmhw
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+TARGET_PROVIDES_LIBLIGHTS := true
 
-# Recovery
-USE_SET_METADATA := false
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
+# FB legacy
+BOARD_EGL_NEEDS_LEGACY_FB := true
+
+# OpenGL
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+# FM
+TARGET_PROVIDES_TI_FM_SERVICE := true
+
+# NFC
+BOARD_HAVE_NFC := true
+
+# Libril
+BOARD_PROVIDES_LIBRIL := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/htc/ruby/sepolicy
+BOARD_SEPOLICY_DIRS += device/htc/ruby/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     app.te \
@@ -107,9 +169,9 @@ BOARD_SEPOLICY_UNION += \
     device.te \
     domain.te \
     drmserver.te \
+    file.te \
     file_contexts \
     files \
-    file.te \
     hci_init.te \
     healthd.te \
     init.te \
@@ -126,85 +188,8 @@ BOARD_SEPOLICY_UNION += \
     wpa.te \
     wpa_socket.te
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := ruby
-
-# Kernel
-BOARD_KERNEL_BASE := 0x48000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=ruby no_console_suspend=1
-BOARD_KERNEL_PAGE_SIZE := 2048
-TARGET_KERNEL_CONFIG := ruby_defconfig
-
-# Wifi
-USES_TI_MAC80211 := true
-ifdef USES_TI_MAC80211
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
-BOARD_WLAN_DEVICE                := wl12xx_mac80211
-BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
-WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
-WIFI_FIRMWARE_LOADER             := ""
-COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-endif
-
-# Required for newer wpa_supplicant_8_ti versions to fix tethering
-#BOARD_WIFI_SKIP_CAPABILITIES := true
-
-WLAN_MODULES:
-	make clean -C hardware/ti/wlan/mac80211/compat_wl12xx
-	make -j8 -C hardware/ti/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
-	mv hardware/ti/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
-
-TARGET_KERNEL_MODULES += WLAN_MODULES
-
-# Bluetooth
-BOARD_WPAN_DEVICE := true
-BOARD_HAVE_BLUETOOTH_TI := true
-
-# FM
-TARGET_PROVIDES_TI_FM_SERVICE := true
-
-# QCOM GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := ruby
-
-# AUDIO
-BOARD_USES_FLUENCE_INCALL := true  # use DMIC in call only
-BOARD_USES_SEPERATED_AUDIO_INPUT := true  # use distinct voice recognition use case
-BOARD_USES_SEPERATED_CAMCORDER := true  # use distinct camcorder use cases
-BOARD_USES_SEPERATED_VOICE_SPEAKER := true  # use distinct voice speaker use case
-
-# NFC
-BOARD_HAVE_NFC := true
-
-# Use libril in the device tree
-BOARD_PROVIDES_LIBRIL := true
-
-# Filesystem
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610611712
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2684354048
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Hardware tunables
+BOARD_HARDWARE_CLASS := device/htc/ruby/cmhw
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
-
-# Recovery
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk1p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TARGET_RECOVERY_FSTAB = device/htc/ruby/ramdisk/fstab.ruby
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
-RECOVERY_FSTAB_VERSION := 2
--- ramdisk/fstab.ruby
+++ ramdisk/fstab.ruby
@@ -1,17 +1,16 @@
-# Android fstab file.
-#<src>						<mnt_point>		<type>	<mnt_flags>						<fs_mgr_flags>
-/dev/block/mmcblk0p22       /boot       emmc  defaults            defaults
-/dev/block/mmcblk0p23       /recovery   emmc  defaults            defaults
-/dev/block/mmcblk0p29       /system     ext4  ro,data=ordered,noauto_da_alloc                   wait
-/dev/block/mmcblk0p30       /cache      ext4  noatime,nosuid,nodev,data=ordered,noauto_da_alloc	wait
-/dev/block/mmcblk0p31       /data       ext4  noatime,nosuid,nodev,data=ordered,noauto_da_alloc	wait
-/dev/block/mmcblk0p32       /devlog     ext4  noatime,nosuid,nodev,data=ordered,noauto_da_alloc	wait
+# System
+/dev/block/mmcblk0p22    /boot        emmc    defaults                defaults
+/dev/block/mmcblk0p23    /recovery    emmc    defaults                defaults
+/dev/block/mmcblk0p29    /system      ext4    rw,noatime,barrier=0    wait
+/dev/block/mmcblk0p30    /cache       ext4    noatime,nosuid,nodev,barrier=0,data=ordered,noauto_da_alloc,journal_async_commit,errors=panic   wait
+/dev/block/mmcblk0p31    /data        ext4    noatime,nosuid,nodev,barrier=0,data=ordered,noauto_da_alloc,journal_async_commit,errors=panic   wait
+/dev/block/mmcblk0p32    /devlog      ext4    noatime,nosuid,nodev,barrier=0,data=ordered,noauto_da_alloc,journal_async_commit,errors=panic   wait
 
 # Internal
-/devices/platform/msm_sdcc.1/mmc_host/mmc0  auto      auto  defaults            voldmanaged=sdcard0:36,nonremovable,noemulatedsd
+/devices/platform/msm_sdcc.1/mmc_host/mmc0    auto    auto            defaults    voldmanaged=sdcard0:36,nonremovable,noemulatedsd
 
 # External
-/devices/platform/msm_sdcc.3/mmc_host/mmc2  auto      auto  defaults            voldmanaged=sdcard1:auto
+/devices/platform/msm_sdcc.3/mmc_host/mmc2    auto    auto            defaults    voldmanaged=sdcard1:auto
 
 # USB Disk
-/devices/platform/msm_hsusb.0/usb1          auto      auto  defaults            voldmanaged=usbdisk:auto
+/devices/platform/msm_hsusb.0/usb1            auto    auto            defaults    voldmanaged=usbdisk:auto
