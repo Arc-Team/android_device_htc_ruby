35b6c0f59074a90bf55455b9cf29de301647d2ec
José Adolfo Galdámez <josegalre@gmail.com>
2014-07-04 12:40:55 -0600

    order in shell files

--- rootdir/init.qcom.sh
+++ rootdir/init.qcom.sh
@@ -11,25 +11,20 @@ usbchgdisabled=`getprop persist.usb.chgdisabled`
 platformvalue=`cat /sys/devices/system/soc/soc0/hw_platform`
 
 # Start ril-daemon only for targets on which radio is present
-case "$baseband" in
-     "msm" | "csfb" | "svlte2a" | "unknown")
-        start ril-daemon
-        start qmuxd
-        case "$baseband" in
-             "svlte2a" | "csfb")
-                start qmiproxy
-        esac
-        case "$multirild" in
-             "true")
-                case "$dsds" in
-                     "true")
-                        start ril-daemon1
-                esac
-        esac
-        case "$netmgr" in
-             "true")
-                start netmgrd
-        esac
+case "$baseband" in "msm" | "csfb" | "svlte2a" | "unknown")
+    start ril-daemon
+    start qmuxd
+    case "$baseband" in "svlte2a" | "csfb")
+        start qmiproxy ;;
+    esac
+    case "$multirild" in "true")
+        case "$dsds" in "true")
+            start ril-daemon1 ;;
+        esac ;;
+    esac
+    case "$netmgr" in "true")
+        start netmgrd ;;
+    esac ;;
 esac
 
 
@@ -43,7 +38,7 @@ done
 case "$serialnum" in
      "") ;;
      * )
-        echo "$serialnum" > /sys/class/android_usb/android0/iSerial ;;
+        echo "$serialnum" >> /sys/class/android_usb/android0/iSerial ;;
 esac
 
 
@@ -52,7 +47,7 @@ esac
 case "$serialno" in
      "") ;;
      * )
-        echo "$serialno" > /sys/class/android_usb/android0/iSerial ;;
+        echo "$serialno" >> /sys/class/android_usb/android0/iSerial ;;
 esac
 
 
@@ -61,43 +56,42 @@ esac
 case "$usbchgdisabled" in
      "") ;;
      * )
-        echo "$usbchgdisabled" > /sys/module/pmic8058_charger/parameters/disabled
-        echo "$usbchgdisabled" > /sys/module/smb137b/parameters/disabled ;;
+        echo "$usbchgdisabled" >> /sys/module/pmic8058_charger/parameters/disabled
+        echo "$usbchgdisabled" >> /sys/module/smb137b/parameters/disabled ;;
 esac
 
 
 # Allow USB enumeration with default PID/VID
-echo 1  > /sys/class/android_usb/f_mass_storage/lun/nofua
+echo 1  >> /sys/class/android_usb/f_mass_storage/lun/nofua
 case "$baseband" in
      "svlte2a")
         start gpsone_daemon
         start bridgemgrd
-        echo 0 > /sys/class/android_usb/android0/enable
-        echo 0x9037 > /sys/class/android_usb/android0/idProduct
-        echo 0x05C6 > /sys/class/android_usb/android0/idVendor
-        echo diag,diag_mdm > /sys/class/android_usb/android0/f_diag/clients
-        echo sdio,smd > /sys/class/android_usb/android0/f_serial/transports
-        echo diag,adb,serial,rmnet_smd_sdio,mass_storage > /sys/class/android_usb/android0/functions
-        echo 1 > /sys/class/android_usb/android0/enable ;;
+        echo 0 >> /sys/class/android_usb/android0/enable
+        echo 0x9037 >> /sys/class/android_usb/android0/idProduct
+        echo 0x05C6 >> /sys/class/android_usb/android0/idVendor
+        echo diag,diag_mdm >> /sys/class/android_usb/android0/f_diag/clients
+        echo sdio,smd >> /sys/class/android_usb/android0/f_serial/transports
+        echo diag,adb,serial,rmnet_smd_sdio,mass_storage >> /sys/class/android_usb/android0/functions
+        echo 1 >> /sys/class/android_usb/android0/enable ;;
      "csfb")
-        echo 0 > /sys/class/android_usb/android0/enable
-        echo 0x9031 > /sys/class/android_usb/android0/idProduct
-        echo 0x05C6 > /sys/class/android_usb/android0/idVendor
-        echo diag,diag_mdm > /sys/class/android_usb/android0/f_diag/clients
-        echo sdio,tty > /sys/class/android_usb/android0/f_serial/transports
-        echo diag,adb,serial,rmnet_sdio,mass_storage > /sys/class/android_usb/android0/functions
-        echo 1 > /sys/class/android_usb/android0/enable ;;
+        echo 0 >> /sys/class/android_usb/android0/enable
+        echo 0x9031 >> /sys/class/android_usb/android0/idProduct
+        echo 0x05C6 >> /sys/class/android_usb/android0/idVendor
+        echo diag,diag_mdm >> /sys/class/android_usb/android0/f_diag/clients
+        echo sdio,tty >> /sys/class/android_usb/android0/f_serial/transports
+        echo diag,adb,serial,rmnet_sdio,mass_storage >> /sys/class/android_usb/android0/functions
+        echo 1 >> /sys/class/android_usb/android0/enable ;;
      *)
-        echo 0 > /sys/class/android_usb/android0/enable
-        echo 0x9025 > /sys/class/android_usb/android0/idProduct
-        echo 0x05C6 > /sys/class/android_usb/android0/idVendor
-        echo diag > /sys/class/android_usb/android0/f_diag/clients
-        echo tty,tty > /sys/class/android_usb/android0/f_serial/transports
-        echo diag,adb,serial,rmnet_smd,mass_storage > /sys/class/android_usb/android0/functions
-        echo 1 > /sys/class/android_usb/android0/enable
-        case "$baseband" in
-             "msm")
-                start port-bridge
+        echo 0 >> /sys/class/android_usb/android0/enable
+        echo 0x9025 >> /sys/class/android_usb/android0/idProduct
+        echo 0x05C6 >> /sys/class/android_usb/android0/idVendor
+        echo diag >> /sys/class/android_usb/android0/f_diag/clients
+        echo tty,tty >> /sys/class/android_usb/android0/f_serial/transports
+        echo diag,adb,serial,rmnet_smd,mass_storage >> /sys/class/android_usb/android0/functions
+        echo 1 >> /sys/class/android_usb/android0/enable
+        case "$baseband" in "msm")
+            start port-bridge ;;
         esac ;;
 esac
 
@@ -107,7 +101,7 @@ case "$platformvalue" in
         if [ ! -s /data/system/sensors/settings ]; then
             # If the settings file is empty, enable sensors
             # Otherwise leave the file with it's current contents
-            echo 1 > /data/system/sensors/settings
+            echo 1 >> /data/system/sensors/settings
         fi
         start sensors
         setprop ro.sf.lcd_density 240
