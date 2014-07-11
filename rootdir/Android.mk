83ceff70a722aa7a5ea2ea0a9958a06a853f152e
JoseGalRe <josegalre@gmail.com>
2014-07-11 14:38:30 -0600

    fix my derp

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -33,9 +33,9 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.sh
+LOCAL_MODULE       := init.ruby.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.qcom.sh
+LOCAL_SRC_FILES    := init.ruby.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
