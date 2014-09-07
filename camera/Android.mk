a3e66e7add01972d714bb8cfaa52ae5548579b10
JoseGalRe <josegalre@gmail.com>
2014-09-07 11:29:21 -0600

    Cleanup makefiles

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,16 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE           := camera.msm8660
-LOCAL_MODULE_TAGS      := optional
-LOCAL_SRC_FILES        := CameraWrapper.cpp
 LOCAL_SHARED_LIBRARIES := libhardware liblog libcamera_client libutils
+LOCAL_SRC_FILES        := CameraWrapper.cpp
+LOCAL_MODULE           := camera.msm8660
 LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_TAGS      := optional
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES :=
+LOCAL_SRC_FILES        :=
 LOCAL_MODULE           := libsurfaceflinger_client
 LOCAL_MODULE_TAGS      := optional
-LOCAL_SRC_FILES        :=
-LOCAL_SHARED_LIBRARIES :=
 include $(BUILD_SHARED_LIBRARY)
