947590e856c0a841587f90153302342dfe7791ef
JoseGalRe <josegalre@gmail.com>
2014-12-03 21:14:33 -0600

    Clean up media codecs
    * Use common Google codec definitions
    
    Change-Id: I9dc78912e19f67f1a441e0081990e41da3ac4ea1

--- ruby.mk
+++ ruby.mk
@@ -156,7 +156,10 @@ PRODUCT_COPY_FILES += \
 # Media
 PRODUCT_COPY_FILES += \
     device/htc/ruby/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    device/htc/ruby/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml
+    device/htc/ruby/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # WiFi
 PRODUCT_COPY_FILES += \
