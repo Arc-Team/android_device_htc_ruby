457bffcb67f6d9585143e42d28e47ce0099442e9
JoseGalRe <josegalre@gmail.com>
2014-12-15 03:26:33 -0600

    Update Device overrides

--- cm.mk
+++ cm.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Device Stuff
 $(call inherit-product, device/htc/ruby/ruby.mk)
 
-# Release name
+# Release Name
 PRODUCT_RELEASE_NAME := ruby
 
 # Bootanimation
@@ -17,5 +17,8 @@ PRODUCT_BRAND := htc
 PRODUCT_MODEL := Amaze 4G
 PRODUCT_MANUFACTURER := HTC
 
-# Set Build Bingerprint / ID / Product Name
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_ruby BUILD_FINGERPRINT=tmous/htc_ruby/ruby:4.4/KRT16M/373265.3:user/release-keys PRIVATE_BUILD_DESC="2.14.531.3_R2 CL373265 release-keys" BUILD_NUMBER=373265
+# Device Overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=ruby \
+    PRIVATE_BUILD_DESC="2.14.531.3_R2 CL373265 release-keys" \
+    BUILD_FINGERPRINT="tmous/htc_ruby/ruby:4.4/KRT16M/373265.3:user/release-keys"
